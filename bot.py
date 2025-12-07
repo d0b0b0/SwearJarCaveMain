7bf58f9d3bef1b3b98a6e6354f1f96ae4ae17352
d0b0b0 <109921993+d0b0b0@users.noreply.github.com>
2025-12-07 12:34:02 +0200

    123

--- bot.py
+++ bot.py
@@ -197,7 +197,7 @@ async def on_message(message: discord.Message):
         save_json(STATS_FILE, stats_data)
 
         try:
-            await message.add_reaction("<:Donald_Trump_official_portrait:1447172263395987496>")
+            await message.add_reaction("<:Donald_Trump_official_portrait:1447173750205186149>")
         except Exception:
             pass
 
